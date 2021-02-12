4dc6ffc9520518d5f7f2a59d8067c9fe00462040
Zhang Chen <59479068+e0397123@users.noreply.github.com>
2021-02-12 10:24:18 +0800

    Update check_dataset.sh
    without checking input, the rm -rf can be dangerous.

--- scripts/check_dataset.sh
+++ scripts/check_dataset.sh
@@ -2,6 +2,18 @@
 DIR_NAME=$1
 TARGET_DIRECTORY=$2
 NAME=$3
+if [ -n "$2" ]; then
+    echo "TARGET_DIRECTORY is set to '$TARGET_DIRECTORY'"
+else
+    echo "TARGET_DIRECTORY is unset"
+    exit 1
+fi
+if [ -n "$3" ]; then
+    echo "NAME is set to '$NAME'"
+else
+    echo "NAME is unset"
+    exit 1
+fi
 if [ -d $TARGET_DIRECTORY/$NAME ]; then
     read -p "Do you want to rebuild the $NAME dataset? " -n 1 -r
     echo
